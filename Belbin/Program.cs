a2679170416f21d42a85514e0096292863f5dbf0
iBroadwell <a.broadwell-2021@hull.ac.uk>
2023-02-06 00:14:17 +0000

    changed to multidimensional arrays for prompts

--- Belbin/Program.cs
+++ Belbin/Program.cs
@@ -1,16 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Belbin Test");
 Console.WriteLine();
-string[] sectionA = 
-{ "1. I can be relied upon to see that work that needs to be done is organised",
+
+
+string[,] prompts = { { "1. I can be relied upon to see that work that needs to be done is organised",
 "2. I pick up slips and omissions that others fail to notice",
-"3. I react strongly when meetings look like losing track of the main",
+"3. I react strongly when meetings look like losing track of the main objective",
 "4. I produce original suggestions",
 "5. I  analyse  other  people’s  ideas  objectively,  for  both  merits  and failings",
 "6. I am keen to find out the latest ideas and developments",
 "7. I have an amplitude for organising people",
-"8. I am always read to support good suggestions that help to resolve a problem"};
-string[] sectionB = 
+"8. I am always read to support good suggestions that help to resolve a problem"},
 { "1. I like to have a strong influence on decisions",
 "2. I feel in my element where work requires a high degree of attention and concentration",
 "3. I am concerned to help colleagues with their problems",
@@ -18,8 +18,7 @@ string[] sectionB =
 "5. I tend to have a creative approach to problem solving",
 "6. I enjoy reconciling different points of view",
 "7. I am more interested in practicalities than new ideas",
-"8. I praticularly enjoy exploring different views and techniques"};
-string[] sectionC = 
+"8. I praticularly enjoy exploring different views and techniques"},
 { "1. I keep a watching eye on areas where difficulty may arise",
 "2. I explore ideas that may have a wider application than in the immediate task",
 "3. I like to weigh up and evaluate a range of suggestions thoroughly before choosing",
@@ -27,8 +26,7 @@ string[] sectionC =
 "5. I maintain a steady systematic approach, whatever the pressures",
 "6. I often produce a new approach to a long continuing problem",
 "7. I am ready to make my personal views known in a forceful way if necessary",
-"8. I am ready to help whenever I can"};
-string[] sectionD =
+"8. I am ready to help whenever I can"},
 { "1. I am keen to see there is nothing vague about my task and objectives",
 "2. I am not reluctant to emphasis my own point of view in meetings",
 "3. I can work with all sorts of people provided that they have got something worthwhile to contribute",
@@ -36,8 +34,7 @@ string[] sectionD =
 "5. I can usually find the argument and refute unsound propositions",
 "6. I tend to see patterns where others would see items as unconnected",
 "7. Being busy gives me real satisfaction",
-"8. I have a quiet interest in getting to know people better"};
-string[] sectionE =
+"8. I have a quiet interest in getting to know people better"},
 { "1. I often find my imagination frustrated by working in a group",
 "2. I find my personal skill particularly appropriate in achieving agreement",
 "3. My feelings seldom interfere with my judgement",
@@ -45,8 +42,7 @@ string[] sectionE =
 "5. I can work with people who vary widely in their personal qualities and outlook",
 "6. I feel it is sometimes worth incurring some temporary unpopularity if no is to succeed in getting one's views across in a group",
 "7. I usually know someone whos specialist knowledge is particularly apt",
-"8. I seem to develop a natural sense of urgency"};
-string[] sectionF =
+"8. I seem to develop a natural sense of urgency"},
 { "1. I start to look around for possible ideas and openings",
 "2. I am concerned to finish and perfect current work before I start",
 "3. I approach the problem in a carefully analytical way",
@@ -54,8 +50,7 @@ string[] sectionF =
 "5. I am able to take an independent and innovative look at most situations",
 "6. I am happy to take the lead when action is required",
 "7. I can respond positively to my colleagues and their initiatives",
-"8. I find it hard to give in a jobw here the goals are not clearly defined"};
-string[] sectionG =
+"8. I find it hard to give in a jobw here the goals are not clearly defined"},
 { "1. I start to look around for possible ideas and openings",
 "2. I am concerned to finish and perfect current work before I start",
 "3. I approach the problem in a carefully analytical way",
@@ -63,57 +58,28 @@ string[] sectionG =
 "5. I am able to take an independent and innovative look at most situations",
 "6. I am happy to take the lead when action is required",
 "7. I can respond positively to my colleagues and their initiatives",
-"8. I find it hard to give in a jobw here the goals are not clearly defined"};
+"8. I find it hard to give in a jobw here the goals are not clearly defined"} };
 
-Console.WriteLine("Section A: WHEN INVOLVED IN A PROJECT WITH OTHER PEOPLE");
-string[] sectionAResultsString = Scoring(sectionA).Split(' ');
+string[] headings = { "Section A: WHEN INVOLVED IN A PROJECT WITH OTHER PEOPLE",
+    "Section B: IN SEEKING SATISFACTION THROUGH MY WORK",
+    "Section C: WHEN THE TEAM IS TRYING TO SOLVE A PRATICULARLY COMPLEX PROBLEM",
+    "Section D: IN CARRYING OUT DAY TO DAY WORK",
+    "Section E: IF I AM SUDDENLY GIVEN A DIFFICULT TASK WITH LIMITED TIME AND UNFAMILAR PEOPLE",
+    "Section F: WHEN SUDDENLY ASKED TO CONSIDER A NEW PROJECT",
+    "Section G: IN CONTRIBUTING TO GROUP PROJECTS IN GENERAL" };
+
+
 int[,] results = new int[7, 8];
-for (int i = 0; i < results.GetLength(1); i++)
+for (int x = 0; x < headings.Length; x++)
 {
-    results[0, i] = int.Parse(sectionAResultsString[i]);
+    Console.WriteLine(headings[x]);
+    string[] sectionAResultsString = Scoring(prompts, x).Split(' ');
+    for (int i = 0; i < results.GetLength(1); i++)
+    {
+        results[x, i] = int.Parse(sectionAResultsString[i]);
+    }
 }
-Console.WriteLine();
-Console.WriteLine("Section B: IN SEEKING SATISFACTION THROUGH MY WORK");
-string[] sectionBResultsString = Scoring(sectionB).Split(' ');
-for (int i = 0; i < results.GetLength(1); i++)
-{
-    results[1, i] = int.Parse(sectionBResultsString[i]);
-}
-Console.WriteLine();
-Console.WriteLine("Section C: WHEN THE TEAM IS TRYING TO SOLVE A PRATICULARLY COMPLEX PROBLEM");
-string[] sectionCResultsString = Scoring(sectionC).Split(' ');
-for (int i = 0; i < results.GetLength(1); i++)
-{
-    results[2, i] = int.Parse(sectionCResultsString[i]);
-}
-Console.WriteLine();
-Console.WriteLine("Section D: IN CARRYING OUT DAY TO DAY WORK");
-string[] sectionDResultsString = Scoring(sectionD).Split(' ');
-for (int i = 0; i < results.GetLength(1); i++)
-{
-    results[3, i] = int.Parse(sectionDResultsString[i]);
-}
-Console.WriteLine();
-Console.WriteLine("Section E: IF I AM SUDDENLY GIVEN A DIFFICULT TASK WITH LIMITED TIME AND UNFAMILAR PEOPLE");
-string[] sectionEResultsString = Scoring(sectionE).Split(' ');
-for (int i = 0; i < results.GetLength(1); i++)
-{
-    results[4, i] = int.Parse(sectionEResultsString[i]);
-}
-Console.WriteLine();
-Console.WriteLine("Section F: WHEN SUDDENLY ASKED TO CONSIDER A NEW PROJECT");
-string[] sectionFResultsString = Scoring(sectionF).Split(' ');
-for (int i = 0; i < results.GetLength(1); i++)
-{
-    results[5, i] = int.Parse(sectionFResultsString[i]);
-}
-Console.WriteLine();
-Console.WriteLine("Section G: IN CONTRIBUTING TO GROUP PROJECTS IN GENERAL");
-string[] sectionGResultsString = Scoring(sectionG).Split(' ');
-for (int i = 0; i < results.GetLength(1); i++)
-{
-    results[6, i] = int.Parse(sectionGResultsString[i]);
-}
+
 
 int[] finalResults = new int[8];
 int[] finalResultsIndex = new int[8];
@@ -242,12 +208,12 @@ bool IsDigitsOrSpace(string str)
 
     return true;
 }
-string Scoring(string[] sectionQuestions)
+string Scoring(string[,] sectionQuestions, int index)
 {
     Console.WriteLine("Choose the 1-3 options that resonate most with you, and assign 10 points in total between those selections");
-    for (int j = 0; j < sectionQuestions.Length; j++)
+    for (int j = 0; j < sectionQuestions.GetLength(1); j++)
     {
-        Console.WriteLine(sectionQuestions[j]);
+        Console.WriteLine(sectionQuestions[index, j]);
     }
     string[] aSectionArray = new string[8];
     int[] aSectionScore = { 0, 0, 0, 0, 0, 0, 0, 0 };
@@ -311,10 +277,10 @@ string Scoring(string[] sectionQuestions)
         aSectionTotal = 0;
         for (int c = 0; c < aSectionArray.Length; c++)
         {
-            Console.WriteLine($"What score would you give statement {aSectionArray[c]}");
+            Console.WriteLine($"What score would you give statement {prompts[c, int.Parse(aSectionArray[c]) - 1]}");
             aSectionScoreString = Console.ReadLine();
             parseSuccess = int.TryParse(aSectionScoreString, out aSectionScore[int.Parse(aSectionArray[c]) - 1]);
-            if (parseSuccess == false || aSectionScore[c] > 10 || aSectionScore[c] < 1)
+            if (parseSuccess == false || aSectionScore[c] > 10 || aSectionScore[c] < 0)
             {
                 Console.WriteLine("Please enter digits");
                 continue;
